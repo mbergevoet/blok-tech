35b74b7849461834c65be0367d0f1b67af045d91
mbergevoet <34296504+mbergevoet@users.noreply.github.com>
2020-03-02 13:41:29 +0100

    ejs server handler working

--- index.js
+++ index.js
@@ -1,36 +1,27 @@
-// const camelCase = require('camelcase');
-// console.log(camelCase('foo-bar')); 
-
-const express = require('express')
-const app = express()
+var express = require('express')
+var app = express()
 const port = 8000;
-const path = require('path');
-
-app.use('/static', express.static('static'));
-
-// function onhome(req, res) {
-//   res.send('<h1>Hello Client</h1>\n')
-// }
-
-// app.get('/', (req, res) =>
-//   res.send('<h1>Hello Client</h1>\n')
-
-app.get('/index', (req, res) =>
-  res.sendfile(path.join(__dirname + 'static/index.html')))
-
-app.get('/myprofile', (req, res) =>
-  res.sendfile(path.join(__dirname + 'static/myprofile.html')))
-
-app.get('/mychats', (req, res) =>
-  res.sendfile(path.join(__dirname + 'static/chats.html')))
-
-app.get('/mp3', (req, res) =>
-  res.sendfile(path.join(__dirname + 'static/chats.html')))
+// var data = [
+//   {
+//     id: 'evil-dead',
+//     title: 'Evil Dead',
+//     plot: 'Five friends travel to a cabin in the …',
+//     description: 'Five friends head to a remote …'
+//   },
+//   {
+//     id: 'the-shawshank-redemption',
+//     title: 'The Shawshank Redemption',
+//     plot: 'Two imprisoned men bond over a number …',
+//     description: 'Andy Dufresne is a young and  …'
+//   }
+// ]
+express()
+  .use(express.static('static'))
+  .set('view engine', 'ejs')
+  
+  .use('/static', express.static('static'))
+  
+app.get('/', (req, res) =>
+  res.render('main.ejs'))
 
 app.listen(port, () => console.log('Example app listening on port ${ port }!'))
-
-
-// http://localhost:8000/static/index.html
-// http://localhost:8000/static/my-profile.html
-// http://localhost:8000/static/my-profile.html
-
