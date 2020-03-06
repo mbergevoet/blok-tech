1a9c82b75dd940446933dd582c93a06b964a294f
mbergevoet <34296504+mbergevoet@users.noreply.github.com>
2020-03-06 12:02:37 +0100

    form almost able to insert data into the server, changed form on searh page and working on collapsable checklist

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-var express = require('express')
-var app = express()
+let express = require('express')
+var slug = require('slug')
+var bodyParser = require('body-parser')
+let app = express()
 const port = 8000;
 
-var data = [
+let data = [
   {
     id: 'evil-dead',
     title: 'Evil Dead',
@@ -19,14 +21,38 @@ var data = [
 
 express()
   .use(express.static('static'))
-  .set('view engine', 'ejs')
-  
+  .use(bodyParser.urlencoded({extended: true}))
   .use('/static', express.static('static'))
+  .set('view engine', 'ejs')
+  .get('/add', form)
+  .post('/', add)
   
 app.get('/', (req, res) =>
   res.render('main.ejs', {data: data}))
 
 app.get('/', (req, res, next) =>
   res.render('details.ejs', {data: data}))
+
+app.get('/', (req, res, next) =>
+  res.render('head.ejs', {data: data}))
+
+app.get('/', (req, res, next) =>
+  res.render('add.ejs', {data: data}))
+
+app.from('/', (req, res) =>
+  res.render('add.ejs'))
+
+function add(req, res) {
+  var id = slug(req.body.title).toLowerCase()
+  console.log(id)
+  data.push({
+    id: id,
+    title: req.body.title,
+    plot: req.body.plot,
+    description: req.body.description
+  })
+
+  res.redirect('/' + id)
+}
 
 app.listen(port, () => console.log(`Example app listening on port ${ port }!`))
