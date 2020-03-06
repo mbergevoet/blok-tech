4c8a64193bb2ac319d9d0c88ba176c58709d7f11
mbergevoet <34296504+mbergevoet@users.noreply.github.com>
2020-03-06 16:43:07 +0100

    added filter dropdown on the result page with transition

--- static/js/script.js
+++ static/js/script.js
@@ -1,25 +1,25 @@
 console.log("Gekoppelt");
 let checkbox = document.querySelector("#checkboxDropdown");
-let dropdown = document.querySelector("#disabledDropdown");
-
+let dropdown = document.querySelector(".disabledDropdown");
+let filterform = document.querySelector("#filterForm");
 //idee om code compacter te maken
 //stop alle id's van de dropdownes en dropdowns in een array
 //filter door de array als er op een knop gedrukt wordt
 //plaatst de dingen in de template functie
 //voer de functie uit
 
-// function toggleDropdown(){
-//     console.log("klik");
-//     disabledDropdown.classList.toggle("active");
-// }
-
-function toggleVisibility() {
+function toggleDropdown(){
     console.log("klik");
-    if(disabledDropdown.style.display == 'block'){
-        disabledDropdown.style.display = 'none';
-    } else {
-        disabledDropdown.style.display = 'block';
-    }
-};
+    dropdown.classList.toggle("active");
+}
 
-checkbox.addEventListener("click", toggleVisibility);
+// function toggleVisibility() {
+//     console.log("klik");
+//     if(disabledDropdown.style.display == 'block'){
+//         disabledDropdown.style.display = 'none';
+//     } else {
+//         disabledDropdown.style.display = 'block';
+//     }
+// };
+
+checkbox.addEventListener("click", toggleDropdown);
