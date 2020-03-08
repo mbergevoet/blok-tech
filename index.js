4f4d3be3c07850bd5c072736e3dcc0e9bdc46966
mbergevoet <34296504+mbergevoet@users.noreply.github.com>
2020-03-08 18:29:29 +0100

    changed index.js

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
 let express = require('express')
-var slug = require('slug')
-var bodyParser = require('body-parser')
+let slug = require('slug')
+let bodyParser = require('body-parser')
 let app = express()
-const port = 8000;
 
-let data = [
+var data = [
   {
     id: 'evil-dead',
     title: 'Evil Dead',
-    plot: 'Five friends travel to a cabin in the …',
-    description: 'Five friends head to a remote …'
+    plot: 'Five friends travel to a cabin in the woods, where they unknowingly release flesh-possessing demons.',
+    description: 'Five friends head to a remote cabin, where the discovery of a Book of the Dead leads them to unwittingly summon up demons living in the nearby woods. The evil presence possesses them until only one is left to fight for survival.'
   },
   {
     id: 'the-shawshank-redemption',
     title: 'The Shawshank Redemption',
-    plot: 'Two imprisoned men bond over a number …',
-    description: 'Andy Dufresne is a young and  …'
+    plot: 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.',
+    description: 'Andy Dufresne is a young and successful banker whose life changes drastically when he is convicted and sentenced to life imprisonment for the murder of his wife and her lover. Set in the 1940’s, the film shows how Andy, with the help of his friend Red, the prison entrepreneur, turns out to be a most unconventional prisoner.'
   }
 ]
 
@@ -25,7 +24,9 @@ express()
   .use('/static', express.static('static'))
   .set('view engine', 'ejs')
   .get('/add', form)
+  .get('/:id', movie)
   .post('/', add)
+  .listen(8000)
   
 app.get('/', (req, res) =>
   res.render('main.ejs', {data: data}))
@@ -33,14 +34,32 @@ app.get('/', (req, res) =>
 app.get('/', (req, res, next) =>
   res.render('details.ejs', {data: data}))
 
-app.get('/', (req, res, next) =>
+app.get('/', (req, res) =>
   res.render('head.ejs', {data: data}))
 
 app.get('/', (req, res, next) =>
   res.render('add.ejs', {data: data}))
 
-app.from('/', (req, res) =>
-  res.render('add.ejs'))
+// app.from('/', (req, res) =>
+//   res.render('add.ejs'))
+
+function form(req, res) {
+  res.render('add.ejs')
+}
+
+function movie(req, res, next) {
+    var id = req.params.id
+    var movie = find(data, function (value) {
+      return value.id === id
+    })
+  
+    if (!movie) {
+      next()
+      return
+    }
+  
+    res.render('detail.ejs', {data: movie})
+}
 
 function add(req, res) {
   var id = slug(req.body.title).toLowerCase()
@@ -55,4 +74,10 @@ function add(req, res) {
   res.redirect('/' + id)
 }
 
-app.listen(port, () => console.log(`Example app listening on port ${ port }!`))
+function listen(port) {
+  console.log(`Example app listening on port ${ port }!`)
+}
+
+// app.listen(port, () => console.log(`Server is on`))
+
+// app.listen(port, () => console.log(`Example app listening on port ${ port }!`))
