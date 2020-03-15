438c6938841e4dc47ca4e75d5a8334faf1792f31
mbergevoet <34296504+mbergevoet@users.noreply.github.com>
2020-03-15 21:21:55 +0100

    mongodb connected and able to add data to the databe but not able to display yet

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const slug = require('slug')
 const bodyParser = require('body-parser')
 const path = require('path')
 const find = require('array-find')
+const urlencodedParser = bodyParser.urlencoded({
+  extended: true
+});
 const multer = require('multer')
 const mongo = require('mongodb')
 const app = express()
@@ -26,15 +29,14 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.use('/static', express.static('static'))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
-app.get('/main', users)
+// app.get('/:id', movie)
+// app.get('/main', users)
+app.get('/add', form)
+app.post('/details', urlencodedParser, add);
 app.get('/error', notFound)
 
-// app.get('/:id', movie)
-// app.get('/add', form)
-// app.post('/details', add)
-
 app.get('/main', (req, res) =>
-  res.render('pages/main.ejs', {data: data}))
+  res.render('pages/main.ejs'))
 
 function users(req, res, next) {
   db.collection('usersCollection').find().toArray(done)
@@ -44,13 +46,66 @@ function users(req, res, next) {
       next(err)
     } else {
       console.log(data)
-      res.render('details.ejs', {data: data})
+      res.render('partials/details.ejs', {data: data})
     } 
   }
 }  
+
+function add(req, res, next){
+  db.collection('usersCollection').insertOne({
+      name: req.body.name,
+      email: req.body.email,
+      age: req.body.age,
+      hobby: req.body.hobby
+  }, done)
+
+  function done(err, data){
+      if (err){
+          next (err)
+      } else{
+          // res.redirect('/details' + data.insertedId)
+          res.render('partials/details.ejs', {data: data})
+          console.log(data)
+      }
+  }
+}
+
+function form (req, res)  {
+    res.render('partials/add.ejs')
+  }
+
+// function find(req, res, next){
+//   var id = req.params.id
+//   db.collection('movies').findOne({
+//       _id: mongo.ObjectID(id)
+//   }, done)
+
+//   function done(err, data){
+//       if (err){
+//           next (err)
+//       } else{
+//           res.render('detail.ejs', {data: data})
+//       }
+ 
+//   }
+
+// function remove(req, res, next) {
+//   var id = req.params.id
+//   db.collection('usersCollection').deleteOne({
+//     _id: mongo.ObjectID(id)
+//   }, done)
+
+//   function done(err) {
+//     if (err) {
+//       next(err)
+//     } else {
+//       res.json({status: 'ok'})
+//     }
+//   }
+// }
 
 function notFound(req, res) {
   res.status(404).render('not-found.ejs')
 }
 
-app.listen(port, () => console.log(`Example app listening on port ${ port }!`))
+app.listen(port, () => console.log(`Server is running succesfully on port ${ port }!`))
