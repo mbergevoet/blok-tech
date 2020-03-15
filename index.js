3a2f7f9a1f693a515e5f01528954cb446367e466
mbergevoet <34296504+mbergevoet@users.noreply.github.com>
2020-03-15 18:26:58 +0100

    fixed server crash

--- index.js
+++ index.js
@@ -2,15 +2,16 @@ const express = require('express')
 const slug = require('slug')
 const bodyParser = require('body-parser')
 const path = require('path')
-const app = express()
 const find = require('array-find')
 const multer = require('multer')
 const mongo = require('mongodb')
+const app = express()
 require('dotenv').config()
-const port = 8000;
+
+const port = 8000
 
 let db = null
-const uri = 'mongodb://' + process.env.DB_URI + ':' + process.env.DB_PORT
+const uri = process.env.DB_URI
 
 mongo.MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }, function (err, client) {
   if (err) {
@@ -20,55 +21,36 @@ mongo.MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true
   db = client.db(process.env.DB_NAME)
 })
 
-function users(req, res, next) {
-  db.collection('collection1').find().toArray(done)
+app.use(express.static('static'))
+app.use(bodyParser.urlencoded({extended: true}))
+app.use('/static', express.static('static'))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.get('/main', users)
+app.get('/error', notFound)
 
+// app.get('/:id', movie)
+// app.get('/add', form)
+// app.post('/details', add)
+
+app.get('/main', (req, res) =>
+  res.render('pages/main.ejs', {data: data}))
+
+function users(req, res, next) {
+  db.collection('usersCollection').find().toArray(done)
+  
   function done(err, data) {
     if (err) {
       next(err)
     } else {
+      console.log(data)
       res.render('details.ejs', {data: data})
     } 
   }
+}  
+
+function notFound(req, res) {
+  res.status(404).render('not-found.ejs')
 }
-
-// app.use(express.static('static'))
-// app.use(bodyParser.urlencoded({extended: true}))
-// app.use('/static', express.static('static'))
-// app.set('views', path.join(__dirname, 'views'))
-// app.set('view engine', 'ejs')
-// // app.get('/:id', movie)
-// app.get('/add', form)
-// app.post('/details', add)
-// app.get('/main', (req, res) =>
-//   res.render('pages/main.ejs', {data: data}))
-
-  
-// function form(req, res) {
-//     res.render('details.ejs')
-//   }
-
-// function add(req, res) {
-//   data.push({
-//     title: req.body.title,
-//     plot: req.body.plot,
-//     description: req.body.description
-//   })
-
-//   res.render('partials/details.ejs', {data: data})
-// }
-
-// function movie(req, res, next){
-//   var id = req.params.id
-//   var movie = find(data, function (value){
-//       return value.id === id;
-//   })
-
-//   if (!movie){
-//       next()
-//       return
-//   }
-//   res.render('add.ejs', {data: data})
-// }
 
 app.listen(port, () => console.log(`Example app listening on port ${ port }!`))
